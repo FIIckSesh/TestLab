b5c501a61f85fcd6690f76da2de2863342687cc1
Aleksandr Chernyshov <achernys@cs.petrsu.ru>
2019-03-29 12:31:08 +0300

    Отчет по тестам

--- app/main.c
+++ app/main.c
@@ -10,7 +10,9 @@ int main()
   double c;
   printf("Введите коэффициенты: \n");
  int red = scanf("%lf %lf %lf", &a, &b, &c);
- if (red!= 3) exit(45);
+ if (red!= 3) {
+     exit(45);
+ }
   val = sqrFunc(a, b, c);
   if (val.count == 2) printf("Корни уравнения: x1 = %.2lf, x2 = %.2lf \n", val.x1, val.x2);
   return 0;
